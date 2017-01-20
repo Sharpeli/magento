702eafb7923ef3b76a82c63e1c05def755e9c2b0
Ubuntu <MySQLtest@test-docker.hrm0usfng3sehgvq1ozz5xbbrg.hx.internal.cloudapp.net>
2017-01-20 13:37:46 +0000

    refactor Dockerfile and docker-entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,15 @@
-# Build magento 2 from ubuntu base
+# Build magento2 from ubuntu base
 FROM ubuntu:16.04
 
-ENV MAGENTO_VERSION 2.1.3
+ENV PACKAGE_URL https://raw.githubusercontent.com/Sharpeli/Packages/master/Magento-CE-2_1_3_tar_gz-2016-12-13-09-08-39.tar.gz
+ENV PACKAGE_NAME Magento-CE-2_1_3_tar_gz-2016-12-13-09-08-39.tar.gz
 
-#Install build-essential, wget, curl, git, supervisor
+# Install build-essential, wget, cron, acl
 RUN apt-get update \
     && apt-get install -y build-essential \
                        wget \
-                       curl \
-		       cron
+		       cron \
+		       acl
 
 # Install apache2 
 RUN apt-get update \ 
@@ -16,56 +17,54 @@ RUN apt-get update \
 
 # Install PHP
    RUN apt-get -y update \
-    && apt-get install -y php7.0 \
-                           libapache2-mod-php7.0 \
-                           php7.0 \
-                           php7.0-common \
-                           php7.0-gd \
-                           php7.0-mysql \
-                           php7.0-mcrypt \
-                           php7.0-curl \
-                           php7.0-intl \
-                           php7.0-xsl \
-                           php7.0-mbstring \
-                           php7.0-zip \
-                           php7.0-bcmath \
-                           php7.0-iconv
+       && apt-get install -y php7.0 \
+			     libapache2-mod-php7.0 \
+		             php7.0 \
+			     php7.0-common \
+			     php7.0-gd \
+			     php7.0-mysql \
+			     php7.0-mcrypt \
+			     php7.0-curl \
+			     php7.0-intl \
+			     php7.0-xsl \
+			     php7.0-mbstring \
+			     php7.0-zip \
+                             php7.0-bcmath \
+                             php7.0-iconv \
+     && sed -i 's/memory_limit = 128M/memory_limit = 2G/' /etc/php/7.0/apache2/php.ini \
+     && sed -i 's/memory_limit = -1/memory_limit = 512M/' /etc/php/7.0/cli/php.ini 
 
-#Install MySQL  nointeractive
+# Install MySQL without interaction
 RUN DEBIAN_FRONTEND=noninteractive apt-get -y install mysql-server mysql-client
 
 # Install Redis
 RUN wget http://download.redis.io/redis-stable.tar.gz \
-    && tar xvzf redis-stable.tar.gz
-
-RUN cd redis-stable \
+    && tar xvzf redis-stable.tar.gz \
+    && rm redis-stable.tar.gz \
+    && cd redis-stable \
     && make \
     && make install
 
-#Install  Compose
-RUN cd && curl -sS https://getcomposer.org/installer | php \
-    && mv composer.phar /usr/local/bin/composer
-
-#Download Mangento and ready to install
+# Download Mangento CE 2.1.3 archive from my github repo and extract it, to download package from your URL, change the ENV PACKAGE_URL and PACKAGE_NAME
 RUN cd /var/www \
     && mkdir magento \
-    && wget https://raw.githubusercontent.com/Sharpeli/Packages/master/Magento-CE-2_1_3_tar_gz-2016-12-13-09-08-39.tar.gz \
-    &&  tar xvzf ./Magento-CE-2_1_3_tar_gz-2016-12-13-09-08-39.tar.gz -C ./magento
+    && wget ${PACKAGE_URL} \
+    && tar -xvzf ${PACKAGE_NAME} -C ./magento \
+    && rm ${PACKAGE_NAME}
 
-# set file permissions
-RUN	chmod -R 755 /var/www/magento/ \
-	&& chmod -R 777 /var/www/magento/app/etc/ \
-        && chmod -R 777 /var/www/magento/var/ \ 
-	&& chmod -R 777 /var/www/magento/pub/media/ \ 
-	&& chmod -R 777 /var/www/magento/pub/static/ 
-
+# Set pre-installation file permissions
+RUN cd /var/www/magento \
+    && find var vendor pub/static pub/media app/etc -type f -exec chmod u+w {} \; \
+    && find var vendor pub/static pub/media app/etc -type d -exec chmod u+w {} \; \
+    && chmod u+x bin/magento 
+    
 COPY magento.conf /etc/apache2/sites-available/
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 COPY session.php /session.php
 COPY page_caching.php /page_caching.php
 COPY cronjobs /cronjobs
 
-# change permissions for docker-entrypoint.sh
+# Change permissions for docker-entrypoint.sh
 RUN chmod +x /docker-entrypoint.sh
 
 EXPOSE 80
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,83 +1,91 @@
 #!/bin/bash
 
-# set the default value
+# Set the default value
 set_default() {
-	local var="$1"
+        local var="$1"
         if [ ! "${!var:-}" ]; then
                  export "$var"="$2"
         fi
 }
 
-# the file env.php doesn't exitst means that magento hasn't been installed 
-if [ ! -f /var/www/magento/app/etc/env.php ]; then 
-	set_default 'ADMIN_FIRSTNAME' 'firstname'
-	set_default 'ADMIN_LASTNAME' 'lastname'
-	set_default 'ADMIN_EMAIL' 'sample@example.com'
-	set_default 'ADMIN_USER' 'root'
-  	set_default 'ADMIN_PASSWORD' 'password1234'
-        set_default 'DB_NAME' 'magento'
-	set_default 'DB_PASSWORD' 'password1234'
-        set_default 'BACKEND_FRONTNAME' 'admin'
-
-	# configure apache2
-	a2ensite magento.conf
-	a2dissite 000-default.conf
-
-	#configure PHP
-	sed -i 's/memory_limit = 128MB/memory_limit = 2G/' /etc/php/7.0/apache2/php.ini
-	a2enmod rewrite
-        phpenmod mcrypt
-      
-	# configure mysql
-        service mysql start
- 	mysqladmin -u root password $DB_PASSWORD
-        mysql -u root -e "create database magento; GRANT ALL ON magento.* TO magento@localhost IDENTIFIED BY 'magento';" --password=$DB_PASSWORD
- 	
-	# install the magento
-        /var/www/magento/bin/magento setup:install --admin-firstname=$ADMIN_FIRSTNAME \
-          					   --admin-lastname=$ADMIN_LASTNAME \
-						   --admin-email=$ADMIN_EMAIL \
-						   --admin-user=$ADMIN_USER \
-						   --admin-password=$ADMIN_PASSWORD \
-						   --db-name=$DB_NAME \
-  						   --db-password=$DB_PASSWORD \
-						   --backend-frontname=$BACKEND_FRONTNAME \
-						   --base-url=$BASE_URL		
-
-	# check if install magento successfully
-	if [ -f /var/www/magento/app/etc/env.php ]; then 
-		# configure redis
-		sed -e "/'save' => 'files',/ {" -e "r /session.php" -e "d" -e "}" -i /var/www/magento/app/etc/env.php
-		sed -e "/);/ {" -e "r /page_caching.php" -e "d" -e "}" -i /var/www/magento/app/etc/env.php
-	fi
-fi
-
-# check if mysql is running
-if ! service mysql status; then
-	service mysql start
-fi
-
-#start redis in the background
+# Start redis in the background
 redis-server --daemonize yes
 
-# set magento2 mode to production 
-/var/www/magento/bin/magento deploy:mode:set production
+# The file env.php doesn't exitst means that magento hasn't been installed yet
+if [ ! -f /var/www/magento/app/etc/env.php ]; then
+        set_default 'ADMIN_FIRSTNAME' 'firstname'
+        set_default 'ADMIN_LASTNAME' 'lastname'
+        set_default 'ADMIN_EMAIL' 'sample@example.com'
+        set_default 'ADMIN_USER' 'root'
+        set_default 'ADMIN_PASSWORD' 'password1234'
+        set_default 'DB_NAME' 'magento'
+        set_default 'DB_PASSWORD' 'password1234'
+        set_default 'BACKEND_FRONTNAME' 'admin'
 
-#set file permissions
-chmod u+x /var/www/magento/bin/magento
-chmod -R 2777 /var/www/magento/var /var/www/magento/pub/media /var/www/magento/pub/static /var/www/magento/app/etc
+        # Configure apache2 and PHP
+        a2ensite magento.conf
+        a2dissite 000-default.conf
+        a2enmod rewrite
+        phpenmod mcrypt
 
-# enable start cron jobs every minutes
+        # Set mysql password and creat table for magento2
+        service mysql start
+        mysqladmin -u root password $DB_PASSWORD
+        mysql -u root -e "create database magento; GRANT ALL ON magento.* TO magento@localhost IDENTIFIED BY 'magento';" --password=$DB_PASSWORD
+
+        cd /var/www/magento
+
+        # Install the magento2
+        bin/magento setup:install --admin-firstname=$ADMIN_FIRSTNAME \
+                                                   --admin-lastname=$ADMIN_LASTNAME \
+                                                   --admin-email=$ADMIN_EMAIL \
+                                                   --admin-user=$ADMIN_USER \
+                                                   --admin-password=$ADMIN_PASSWORD \
+                                                   --db-name=$DB_NAME \
+                                                   --db-password=$DB_PASSWORD \
+                                                   --backend-frontname=$BACKEND_FRONTNAME \
+                                                   --base-url=$BASE_URL
+
+        # Check if magento installed successfully
+        if [ -f app/etc/env.php ]; then
+
+                # Configure magento2 to use redis
+                sed -e "/'save' => 'files',/ {" -e "r /session.php" -e "d" -e "}" -i app/etc/env.php
+                sed -e "/);/ {" -e "r /page_caching.php" -e "d" -e "}" -i app/etc/env.php
+
+                # switch the magento2 mode to production, we will enable this process until it can proceed on azure web app for linux
+                # echo "switch magento to production mode..."
+                # bin/magento deploy:mode:set production
+        fi
+fi
+
+# Check if mysql is running or start it
+if ! service mysql status; then
+        service mysql start
+fi
+
+cd /var/www/magento
+
+echo "run cron jobs the first time..."
+php bin/magento cron:run
+php bin/magento cron:run
+
+echo "schedule cron jobs to run every minute..."
 service cron restart
 crontab -l > magentocron
-cat cronjobs >> magentocron
+cat /cronjobs >> magentocron
 crontab magentocron
 rm magentocron
 
-echo "Waiting cron jobs to start..."
-while [ ! -f /var/www/magento/var/.setup_cronjob_status ]; do
-       sleep 1s
-done
+echo "Changing file permissions..."
+umask u=,g=,o=
+chmod g+s var
+setfacl -R -d -m g::rwx var
+setfacl -R -d -m o::rwx var
+chmod -R 777 vendor
+chmod -R 777 app/etc
+chmod -R 777 var
+chmod -R 777 pub/static
 
-# run apache in the foreground
+# Run apache in the foreground
 apachectl -DFOREGROUND
