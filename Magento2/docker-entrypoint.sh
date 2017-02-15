0178e9ef470d447c8c9b1205495c11d954e22295
root <root@test-docker.hrm0usfng3sehgvq1ozz5xbbrg.hx.internal.cloudapp.net>
2017-02-15 09:54:25 +0000

    add an waiting page when magento is installing

--- Magento2/docker-entrypoint.sh
+++ Magento2/docker-entrypoint.sh
@@ -93,18 +93,18 @@ if [ ! -f app/etc/env.php ]; then
 		rm /env-tmp.php		 
 
 		if [ "${PRODUCTION_MODE,,}" = "true" ] ; then
-		
-			echo "setting file permissions for production mode..."
-                        find var vendor lib pub/static pub/media app/etc -type f -exec chmod g+w {} \;
-                        find var vendor lib pub/static pub/media app/etc -type d -exec chmod g+w {} \;
-                        chmod o+rwx app/etc/env.php
 	
         	        # Switch the magento2 mode to production, we will enable this process until it can proceed on azure web app for linux
                		echo "switch magento to production mode..."
                 	bin/magento deploy:mode:set production
-
-			# disable the magento maintance mode
+			
+			# Disable the magento maintance mode
                         bin/magento maintenance:disable
+
+			echo "setting file permissions for production mode..."
+                        find var vendor lib pub/static pub/media app/etc -type f -exec chmod g+w {} \;
+                        find var vendor lib pub/static pub/media app/etc -type d -exec chmod g+w {} \;
+                        chmod o+rwx app/etc/env.php
 		else
 			# Default mode
 			echo "setting file permissions for default mode..."
